53ac9d38d6bb281f7be6f471e9d13410bcf64e1d
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-07-18 20:28:50 +0700

    Added a render for when the location is N/A

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import { useGeolocated } from "react-geolocated";
 import weatherAPI from "./scripts/api";
 
 function App() {
-//! States
+  //! States
   const [location, setLocation] = useState({});
   const [areasArr, setAreasArr] = useState([]);
+  let sortedAreasArr;
 
-//! Functions
+  //! Functions
   //* API call to get data of all areas in Indonesia
   useEffect(() => {
     function fetchAPI() {
@@ -18,7 +19,6 @@ function App() {
         .get("/cuaca/wilayah.json")
         .then((response) => {
           setAreasArr(response.data);
-          document.querySelector(".loading").classList.add("inactive");
         })
         .catch((err) => console.log(err));
     }
@@ -34,35 +34,45 @@ function App() {
       userDecisionTimeout: 5000,
     });
 
-  //* Get an array of areas with distance less than 0.5
-  const sortedAreasArr = areasArr.filter((area) => {
-    return (
-      //? Use the phytagorean formula you learnt in junior high school
-      Math.sqrt(
+  if (coords) {
+    //* Get an array of areas with distance less than 0.5
+    sortedAreasArr = areasArr.filter((area) => {
+      return (
+        //? Use the phytagorean formula you learnt in junior high school
+        Math.sqrt(
+          (area.lon - coords.longitude) ** 2 + (area.lat - coords.latitude) ** 2
+        ) < 0.5
+      );
+    });
+
+    //* Sort the array to get the closest areas
+    sortedAreasArr.sort((area) => {
+      return -Math.sqrt(
         (area.lon - coords.longitude) ** 2 + (area.lat - coords.latitude) ** 2
-      ) < 0.5
-    );
-  });
+      );
+    });
 
-  //* Sort the array to get the closest areas
-  sortedAreasArr.sort((area) => {
-    return -Math.sqrt(
-      (area.lon - coords.longitude) ** 2 + (area.lat - coords.latitude) ** 2
+    //* Render
+    return (
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <CoordinateCalculation
+            coords={coords}
+            sortedAreasArr={sortedAreasArr}
+          />
+        </main>
+      </div>
     );
-  });
-
-  //* Render
-  return (
-    <div className="App">
-      <div className="loading bg-slate-600 flex justify-center items-center text-white">Loading</div>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <CoordinateCalculation coords={coords} sortedAreasArr={sortedAreasArr}/>
-      </main>
-    </div>
-  );
+  } else {
+    return(
+      <div>
+        <h1 className="text-3xl text-center text-red-700 bg-red-200">Location unavailable</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
