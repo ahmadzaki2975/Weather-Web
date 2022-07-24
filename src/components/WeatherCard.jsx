9c7dfa0c4bf097c80e953361f51ddf11e7f38a90
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-07-24 17:35:03 +0700

    Changed card opacity

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,6 @@
 export const WeatherCard = (props) => {
   return(
-    <div className="bg-gray-100 rounded-lg h-full p-5 flex flex-col items-center border-white border-2">
+    <div className="card rounded-lg h-full p-5 flex flex-col items-center border-white border-2">
       <h1 className="hidden md:inline">
         {props.date}
       </h1>
